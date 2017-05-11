379030060e02d1868ebb20b37980cc38b7cf5b13
mackdelton <markjdalton@gmail.com>
2017-05-10 21:37:56 -0400

    Update clean_visualize_mass.py

--- clean_visualize_mass.py
+++ clean_visualize_mass.py
@@ -4,15 +4,16 @@ Created on Sun Jan  1 14:50:07 2017
 """
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 
-def parse_mab_input(para_in):
+def parse_mab_input(data, test):
     # Extract relevant dataset pertaining to test scenario
     # Try to concatenate portions of data_in relevant to the scenario of interest
-    data_parse = data_in.loc[(data_in['bernCnt'] == test_in[0]) & (data_in['distribution'] == test_in[3]) &
-                             (data_in['soc'] == test_in[1]) & (data_in['loc_cnt'] == test_in[4]) &
-                             (data_in['iter'] == test_in[2]) & (data_in['sol_type'] == test_in[5]),
-                             colT].reset_index(drop=True)
+    data_parse = data.loc[(data['bern_cnt'] == test[0]) & (data['distribution'] == test[3]) &
+                          (data['soc'] == test[1]) & (data['loc_cnt'] == test[4]) &
+                          (data['iter'] == test[2]) & (data['sol_type'] == test[5]),
+                          colT].reset_index(drop=True)
 
     # Generate new dataframe that stores statistics of each candidate location.
     # Initialize dataframe with candidate locations and append 1) the resulting output vector (variable length)
@@ -62,7 +63,7 @@ soc = 0  # Type of Success of Communication curve (0 - Gamma, 1 - Exponential)
 # nIter = 100  #Number of iterations (timesteps or duration, 100, 500, or 1000)
 nIter = (100, 500, 1000)
 
-soln = 1     # Solution of interest (Gittins Index, GI - 0, Uninformed Random, UR - 1; Educated Guess, EG - 2)
+soln = 0     # Solution of interest (Gittins Index, GI - 0, Uninformed Random, UR - 1; Educated Guess, EG - 2)
 # soln = (0, 1, 2)
 
 distrT = 0    # Distribution type (Uniform Random - 0, Mesh grid - 1)
@@ -88,23 +89,36 @@ locs = pd.read_table(locsFileIn, header=None, sep=',', names=['alocx', 'alocy', 
 
 # Create string for input file
 dataFileIn = './improv_testdata21APR17_stationaryB_'+str(bStationary)+'.txt'
-colT = ['id', 'selx', 'sely', 'rngP', 'rng', 'out', 'bernCnt', 'distribution', 'soc', 'loc_cnt',
+colT = ['id', 'selx', 'sely', 'rngP', 'rng', 'out', 'bern_cnt', 'distribution', 'soc', 'loc_cnt',
         'iter', 'sol_type', 'dist_tot']  # Define the column headers for data extraction
 data_in = pd.read_table(dataFileIn, header=None, sep=',', index_col=False, names=colT)
 
 # Change data types of specific columns (i.e., float for some columns and int for others)
 data_in[['selx', 'sely', 'rngP', 'rng']] = data_in[['selx', 'sely', 'rngP', 'rng']].astype(float)
-data_in[['id', 'out', 'bernCnt', 'distribution', 'soc', 'loc_cnt', 'iter', 'sol_type',
-        'dist_tot']] = data_in[['id', 'out', 'bernCnt', 'distribution', 'soc', 'loc_cnt',
+data_in[['id', 'out', 'bern_cnt', 'distribution', 'soc', 'loc_cnt', 'iter', 'sol_type',
+        'dist_tot']] = data_in[['id', 'out', 'bern_cnt', 'distribution', 'soc', 'loc_cnt',
                                'iter', 'sol_type', 'dist_tot']].astype(int)
-
+locs_stat = []
+data_stat = []
 # Testing different sets of conditions
 for b in bC:
     for nI in nIter:
         # Define tuple of inputs to parsing function
         test_in = (b, soc, nI, distrT, N, soln)
-        locs_stat, data_stat = parse_mab_input(test_in)
+        locs_stat, data_stat = parse_mab_input(data_in, test_in)
         success_total = locs_stat.loc[:, 'success'].astype(int).sum(axis=0)
         trial_total = locs_stat.loc[:, 'trials'].astype(int).sum(axis=0)
         print("%% Success: %6.2f --- Distance: %7.2f\n" % ((100*success_total/trial_total),
                                                            data_stat.loc[1, 'dist_tot']))
+print(data_in)
+print(data_stat)
+toplot = data_stat[['id']].copy()
+toplot.reset_index(level=0, inplace=True)
+
+scat = toplot.plot.scatter(x='index', y='id')
+scat.set_xlim(0, 2000)
+scat.set_ylim(0, 20.5)
+
+line = toplot.plot(x='index', y='id')
+
+plt.show()
